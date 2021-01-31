16060a15e2c13297f2e1512f1bea80a731837c07
congliu <37121298@qq.com>
2021-01-31 22:03:57 +0800

    more patients

--- scrips/run_organizeMCDir.sh
+++ scrips/run_organizeMCDir.sh
@@ -2,6 +2,6 @@ set -ex
 
   #--patient_ID  Lung_Zhengyurui_skin \
 python ./pyRad/organize_MontelCarlo_dir.py \
-  --patient_ID Lung_Zhengyurui_skin \
+  --patient_ID Lung_Liujinzhu_skin \
   --exp_name tmp \
-  --MCDose_shape 86,256,256 \
+  --MCDose_shape 102,256,256 \
